6539c9414d272bc33eac30e008374c3c8168f68f
Graeme Hedges <graeme.hedges@gmail.com>
2025-02-19 19:21:56 +0000

    Minor: Initial Commit

---
+++ test/SpendingAnalyzer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SpendingAnalyzer.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
